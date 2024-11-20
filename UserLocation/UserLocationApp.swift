70e555a54d0369011772966fc341d1c4eb28b3a2
CarlosZR <carlos.z.r.78@gmail.com>
2024-11-20 11:48:57 +0100

    First commit.

---
+++ UserLocation/UserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLocationApp.swift
+//  UserLocation
+//
+//  Created by CarlosZR on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
